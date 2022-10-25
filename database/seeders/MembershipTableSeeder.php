e14beb5909eb7ffc1571aeeb815d959b07d92295
Bhattacharyya-Avi <bhattacharyya4200@gmail.com>
2022-10-25 16:07:23 +0600

    role membership table added

---
+++ database/seeders/MembershipTableSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Membership;
+use Illuminate\Support\Str;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class MembershipTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $data = [
+            [
+                'name' => 'Premium',
+                'slug' => Str::slug('Premium')
+            ],
+            [
+                'name' => 'Free',
+                'slug' => Str::slug('Free')
+            ],
+        ];
+
+        foreach ($data as $key => $value) {
+            Membership::create($value);
+        }
+    }
+}
